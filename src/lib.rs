f6a9de3d27fe4b06b4d51a0a65f0769f20fcf942
whotterre <ijeddy5.0@gmail.com>
2026-05-25 18:06:02 +0100

    feat: implement UTXO addition in add_utxo function

--- src/lib.rs
+++ src/lib.rs
@@ -72,9 +72,10 @@ pub fn normalize_address(address: &str) -> String {
 }
 
 /// Append a new UTXO to the list and return the updated list.
-pub fn add_utxo(utxos: Vec<Utxo>, new_utxo: Utxo) -> Vec<Utxo> {
+pub fn add_utxo(mut utxos: Vec<Utxo>, new_utxo: Utxo) -> Vec<Utxo> {
     // TODO: Push new_utxo into utxos and return it
-    todo!()
+    utxos.push(new_utxo);
+    utxos
 }
 
 /// Find the first transaction with a fee greater than 0.005 BTC.
